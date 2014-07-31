b2ffd902bfa1426e51a1d6452ddf20d4a159c370
Samuel Ko <samuelsko@gmail.com>
2014-07-30 21:45:42 -0700

    fixed firebase images

--- Chrome Extension/js/extensions.js
+++ Chrome Extension/js/extensions.js
@@ -4,36 +4,36 @@
 *
 */
 
-Firebase.enableLogging(true);
-var f = new Firebase("https://torid-fire-9403.firebaseio.com/");
-console.log(f);
+    Firebase.enableLogging(true);
+    var f = new Firebase("https://flickering-fire-2908.firebaseio.com/");
+      var bg = chrome.extension.getBackgroundPage();
 
-// Set up context menu tree at install time.
-chrome.runtime.onInstalled.addListener(function() {
-  // Create one test item for each context type.
-  var contexts = ["image"];
-  for (var i = 0; i < contexts.length; i++) {
-    var context = contexts[i];
-    chrome.tabs.getSelected(null, function(tab) {
-       tabId = tab.id;
-       imgUrl = tab.src; 
-       
-    });
-        var id = chrome.contextMenus.create({"title": "Save your meme", "contexts":[context], "id": "context_save" + context});
-        
-  }
-  
-  
-
-});
-
-//binds chrome onclicked
-chrome.contextMenus.onClicked.addListener(function(info) {
-    alert(info.srcUrl);
-    f.set({
-        src: srcUrl
-    });
-    console.log("successfully set");
+      bg.console.log("huehuehue0");
+  // Set up context menu tree at install time.
+  chrome.runtime.onInstalled.addListener(function() {
+    // Create one test item for each context type.
+    var contexts = ["image"];
+    for (var i = 0; i < contexts.length; i++) {
+      var context = contexts[i];
+      chrome.tabs.getSelected(null, function(tab) {
+         tabId = tab.id;
+         imgUrl = tab.src; 
+         
+      });
+          var id = chrome.contextMenus.create({"title": "Save your meme", "contexts":[context], "id": "context_save" + context});
+          
+    }
     
-});
+    
 
+  });
+
+  //binds chrome onclicked
+  chrome.contextMenus.onClicked.addListener(function(info) {
+      alert(info.srcUrl);
+      f.set({
+        src: info.srcUrl;       
+      });
+      chrome.extension.getBackgroundPage().console.log("successfully set");
+      
+  });  
